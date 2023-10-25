f0ca8fa0971668358053ef77ca90042d4523f780
Christian Caso <71586554@continental.edu.pe>
2023-10-25 16:02:06 -0500

    Agregando MVC Prendas

---
+++ database/migrations/2023_10_25_204805_create_prendas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prendas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 255);
+            $table->string('categoria', 100);
+            $table->string('talla', 100);
+            $table->string('color', 100);
+            $table->text('descripcion');
+            $table->text('imagen');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prendas');
+    }
+};
