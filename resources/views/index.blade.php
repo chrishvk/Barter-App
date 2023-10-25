2b6c82359d9c1fce281a8709fd1f911e481861b4
Christian Caso <71586554@continental.edu.pe>
2023-10-25 15:45:13 -0500

    Agregando Administradores v1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- SLICK --}}
+    <link rel="stylesheet" type="text/css" href="https://kenwheeler.github.io/slick/slick/slick.css">
+    <link rel="stylesheet" type="text/css" href="https://kenwheeler.github.io/slick/slick/slick-theme.css">
     {{-- Animate CSS --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
     {{-- Icono del sistema --}}
@@ -13,56 +16,20 @@
     {{-- Bootstrap 5 --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     {{-- Jquery --}}
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-    <title>Barter App</title>
-
-    <style>
-        body {
-            background: url('https://cdn.pixabay.com/photo/2013/08/28/11/47/leaf-176722_1280.jpg');
-            z-index: -3;
-            background-attachment: fixed;
-            /* Si la imagen de fondo no abarca todo el fondo */
-            background-repeat: no-repeat;
-            background-size: 100vw 100vh;
-        }
-        /* Llamar una fuente extern */
-        /* @font-face {
-            font-family: monospace;
-            src: url('');
-        } */
-        .titulo1 {
-            color: #fff;
-            font-size: 40px;
-        }
-        .btn-fb {
-            background-color: #0a58ca;
-            color: #fff;
-            border-radius: 50%;
-        }
-        .btn-fb:hover {
-            background-color: #fff;
-            color: #0a58ca;
-        }
-        .btn-wpp {
-            background-color: #1cb478;
-            color: #fff;
-            border-color: #1cb478;
-            border-radius: 50%;
-        }
-        .btn-wpp:hover {
-            background-color: #fff;
-            border-color: #1cb478;
-            color: #1cb478;
-        }
-    </style>
-    
+    <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+    {{-- Hoja de estilos --}}
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <title>Barter App</title>   
 
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <div style="width: 45px">
+                <img src="{{asset('img/icono.jpg')}}" alt="logoba" width="35px">
+            </div>
+            <a class="navbar-brand" href="#">BarterApp</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -71,12 +38,10 @@
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#"><i class="bi bi-house-door-fill"></i> Inicio</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
+                        <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-bell-fill"></i>
+                        Notificaciones
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <li><a class="dropdown-item" href="#">Action</a></li>
@@ -86,17 +51,28 @@
                         </ul>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        <a class="nav-link active" href="#"><i class="bi bi-cloud-upload-fill"></i>
+                        Subir fotos</a>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-archive"></i>
+                        Categorías
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Pantalones</a></li>
+                            <li><a class="dropdown-item" href="#">Camisas</a></li>
+                            <li><a class="dropdown-item" href="#">Casaca</a></li>
+                            <li><a class="dropdown-item" href="#">Zapatos</a></li>
+                            <li><a class="dropdown-item" href="#">Camiseta</a></li>
+                            <li><a class="dropdown-item" href="#">Carteras</a></li>
+                            <li><a class="dropdown-item" href="#">Short</a></li>
+                            <li><a class="dropdown-item" href="#">Gorros</a></li>
+                            <li><a class="dropdown-item" href="#">Blusas</a></li>
+                        </ul>
                     </li>
                 </ul>
-                <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-                <div class="d-flex ml-10">
-                    <button class="btn btn-outline-success">Inciar sesión</button>
+                <div class="d-flex">
+                    <button class="btn btn-primary" type="submit">Iniciar sesión</button>
                 </div>
-
             </div>
         </div>
     </nav>
@@ -106,15 +82,167 @@
             <h1 class="titulo1 animate__animated animate__lightSpeedInLeft">Bienvenido a Barter App</h1>
             <button class="btn btn-primary btn-lg btn-fb animate__animated animate__zoomInLeft animate__delay-0.5s"><i class="bi bi-facebook"></i></button>
             <button class="btn btn-primary btn-lg btn-wpp animate__animated animate__zoomInRight animate__delay-0.5s"><i class="bi bi-whatsapp"></i></button>
-
+            <br><br>
+            <div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-6">
+                    <form class="d-flex">
+                        <label for=""><b style="color: #fff; font-size:25px">Buscar: </b></label>
+                        <input class="form-control me-2" type="search" placeholder="Nombre de la prenda" aria-label="Search">
+                        <button class="btn btn-primary" type="submit">
+                            <i class="bi bi-search"></i>
+                        </button>
+                    </form>
+                </div>
+                <div class="col-md-3"></div>
+            </div>
         </center>
+    </div>
+
+    <br><br>
+
+    <div class="container" style="background-color: #CBFFAA; border-radius: 10px">
+        <h3><p style="text-align: center"><b>Recién agregados</b></p></h3>
+        <div class="row">
+            <section class="regular slider" style="margin-top: 0px">
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>            
+            </section>
+        </div>
+    </div>
+
+    <br><br>
+
+    <div class="container" style="background-color: #CBFFAA; border-radius: 10px">
+        <h3><p style="text-align: center"><b>Los más buscados</b></p></h3>
+        <div class="row">
+            <section class="regular slider" style="margin-top: 0px">
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>
+                <div>
+                    <img src='https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwe77c5b62/images/hi-res/196150118259_1_20221215120000-mrtPeru.jpg' height="200px" width="10px">
+                </div>            
+            </section>
+        </div>
     </div>
 
 
 
       
 
-      {{-- Bootstrap 5 --}}
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    {{-- Bootstrap 5 --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    {{-- SLICK script --}}
+    <script src="https://kenwheeler.github.io/slick/slick/slick.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        $(".regular").slick({
+            dots: true,
+            infinite: true,
+            slidesToShow: 5,
+            slidesToScroll: 5
+        });
+    </script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+// Rutas para el administrador
+Route::get('/admin', function() {
+    return view('admin.index');
+});
